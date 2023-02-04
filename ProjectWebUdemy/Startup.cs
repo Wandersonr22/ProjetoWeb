ec6f218724f9ba82a6288288e2ea5949b067ed0c
wanderson rodrigo <wanderson_rodriguis@hotmail.com>
2023-02-04 16:02:30 -0300

    Integracao com mysql

--- ProjectWebUdemy/Startup.cs
+++ ProjectWebUdemy/Startup.cs
@@ -37,7 +37,9 @@ namespace ProjectWebUdemy
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<ProjectWebUdemyContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("ProjectWebUdemyContext")));
+            options.UseMySql(Configuration.GetConnectionString("ProjectWebUdemyContext"), builder =>
+            builder.MigrationsAssembly("ProjectWebUdemy")));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
