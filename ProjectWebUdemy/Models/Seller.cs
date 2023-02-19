6515eca15af6166d3d2c9887f26e184b1645a6aa
wanderson rodrigo <wanderson_rodriguis@hotmail.com>
2023-02-19 18:45:21 -0300

    validacao

--- ProjectWebUdemy/Models/Seller.cs
+++ ProjectWebUdemy/Models/Seller.cs
@@ -9,17 +9,31 @@ namespace ProjectWebUdemy.Models
     public class Seller
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "{0} obrigatorio")]
+        [StringLength(60,MinimumLength =  3, ErrorMessage = "Tamanho do {0} nome deve ser entre {2} e {1} caracteres")]
         public string Name { get; set; }
+
         [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "{0} obrigatorio")]
+        [EmailAddress(ErrorMessage = "Colocar um {0} Valido")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "{0} obrigatorio")]
         [Display(Name = "Birth Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
+
+        [Required(ErrorMessage = "{0} obrigatorio")]
+        [Range(100.0, 50000.00, ErrorMessage = "{0} deve ser no minimo {1} até {2}")]
         [Display(Name = "Base Salary")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
         public double BaseSalary { get; set; }
+
         public Department Department { get; set; }
+
+        [Display(Name = "Department")]
         public int DepartmentId { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
